23714201ca14c462b8924b9af49781a65fc9e09a
dominik34s <dominik.boruta@stud.fhgr.ch>
2026-05-26 20:18:27 +0200

    Live value Animation + Cache
    Es gibt jetzt einen Cache!! und darum wird die API auch seltener gerufen. Bin sehr zufrieden :))))

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 /*Metals API Gold (bis jetzt)*/
 
-const METALS_DEV_API_KEY = 'CWFSTV98PJLHWOHXULAB117HXULABDOMI';
+const METALS_DEV_API_KEY = 'CWFSTV98PJLHWOHXULAB117HXULAB';
 
 const METALS =[
     {code: 'XAU', name: 'Gold'},
@@ -23,24 +23,58 @@ async function fetchMetalPrice(code) {
     const price = json.metals[metalName];
     
     return { price: price, change: null, changePc: null, gram24k: null };
+
+}
+const cache = {
+    XAU: {value: null, lastFetched: null},
+    XAG: {value: null, lastFetched: null},
+    XPT: {value: null, lastFetched: null},
+    XPD: {value: null, lastFetched: null}
 }
 
-async function displayPrices(code) {
+    
+async function displayNewPrices(code) {
     try {
         const data = await fetchMetalPrice(code);
+        cache[code].value = data;
+        cache[code].lastFetched = Date.now();   
         
         const priceElement = document.querySelector('.price-value');
         
         if (priceElement) {
             priceElement.textContent = data.price.toFixed(2);
+            console.log("Displayed new price");
         }
     } catch (error) {
         console.error("Error fetching prices:", error);
     }
 }
 
+async function displayCachedPrices(code) {
+    try {
+        const data = cache[code].value;
+        
+        const priceElement = document.querySelector('.price-value');
+        
+        if (priceElement) {
+            priceElement.textContent = data.price.toFixed(2);
+            console.log("Displayed cached price");
+        }
+    } catch (error) {
+        console.error("Error fetching prices:", error);
+    }
+}
+
+
+const tenMinutes = 10 * 60 * 1000;
+
 document.addEventListener('DOMContentLoaded', () => {
-    displayPrices('XAU'); 
+    if (cache.XAU.value === null || Date.now() - cache.XAU.lastFetched > tenMinutes) {
+        displayNewPrices('XAU'); 
+    }
+    else {
+        displayCachedPrices('XAU');
+    }
 });
 
 /*Buttons wechseln price-container*/
@@ -64,7 +98,12 @@ buttons.forEach(function(button) {
 
         metalName.textContent = button.querySelector('.metal-icon').textContent;
         let code = metalToCode[metall];
-        displayPrices(code);
+        if (cache[code].value === null || Date.now() - cache[code].lastFetched > tenMinutes) {
+            displayNewPrices(code); 
+        }
+         else {
+            displayCachedPrices(code);
+        }
         console.log(button.dataset.metal);
     });
 });
