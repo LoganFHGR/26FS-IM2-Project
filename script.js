476fa2434044c4a45339fb7609ea9b9540486103
LoganFHGR <logan.stein@stud.fhgr.ch>
2026-05-27 09:53:47 +0200

    graph added

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-
 /*Metals API Gold (bis jetzt)*/
 
-const METALS_DEV_API_KEY = 'CWFSTV98PJLHWOHXULAB117HXULAB';
+const METALS_DEV_API_KEY = "CWFSTV98PJLHWOHXULAB117HXULABDOMI";
 
-const METALS =[
-    {code: 'XAU', name: 'Gold'},
-    {code: 'XAG', name: 'Silber'},
-    {code: 'XPT', name: 'Platin'},
-    {code: 'XPD', name: 'Palladium'}
-]
+const METALS = [
+  { code: "XAU", name: "Gold" },
+  { code: "XAG", name: "Silber" },
+  { code: "XPT", name: "Platin" },
+  { code: "XPD", name: "Palladium" },
+];
 
 async function fetchMetalPrice(code) {
-    const codeToName = { 'XAU': 'gold', 'XAG': 'silver', 'XPT': 'platinum', 'XPD': 'palladium' };
-    const metalName = codeToName[code];
+  const codeToName = {
+    XAU: "gold",
+    XAG: "silver",
+    XPT: "platinum",
+    XPD: "palladium",
+  };
+  const metalName = codeToName[code];
 
-    const url = `https://api.metals.dev/v1/latest?api_key=${METALS_DEV_API_KEY}&currency=USD&unit=toz`;
-    const res = await fetch(url);
-    
-    if (!res.ok) throw new Error(`HTTP ${res.status}`);
-    const json = await res.json();
+  const url = `https://api.metals.dev/v1/latest?api_key=${METALS_DEV_API_KEY}&currency=USD&unit=toz`;
+  const res = await fetch(url);
 
     const price = json.metals[metalName];
     
@@ -48,7 +49,11 @@ async function displayNewPrices(code) {
     } catch (error) {
         console.error("Error fetching prices:", error);
     }
-}
+  } 
+  
+  catch (error) {
+    console.error("Error fetching prices:", error);
+  }
 
 async function displayCachedPrices(code) {
     try {
@@ -79,17 +84,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
 /*Buttons wechseln price-container*/
 
-let buttons = document.querySelectorAll('.metal-btn');
+let buttons = document.querySelectorAll(".metal-btn");
 
-buttons.forEach(function(button) {
-    button.addEventListener('click', function() {
-        const metalToCode = {
-            'gold': 'XAU',
-            'silver': 'XAG',
-            'platinum': 'XPT',
-            'palladium': 'XPD'
-        }
+buttons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    const metalToCode = {
+      gold: "XAU",
+      silver: "XAG",
+      platinum: "XPT",
+      palladium: "XPD",
+    };
 
+    let metall = button.dataset.metal;
+    let container = document.getElementById("price-container");
+    let metalName = document.querySelector(".metal-name");
+    container.className = "price-container-" + metall;
 
         let metall = button.dataset.metal
         let container = document.getElementById('price-container');
